25ceee6e70b23edd1c0be92da3723a93b72a9c53
Robin Millette <robin@millette.info>
2018-12-16 15:55:12 -0500

    Initial commit.

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import $ from 'jquery'
+import myObj from './data.json'
+
+$(function(){
+	$("a.hideMe").click(function(){ $("#aR0").hide(); });
+	$("a.showMe").click(function(){ $("#aR0").show(); });
+
+  document.getElementById(myObj.nomMachine).innerHTML = "<a href='" + myObj.urlCanonique + "'>" + myObj.nom + "</a>";
+});
