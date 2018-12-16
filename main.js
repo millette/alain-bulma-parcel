a1be094c5adddd8d089cfb3f3f57be764b9d87a9
Robin Millette <robin@millette.info>
2018-12-16 15:59:31 -0500

    Prettier main.js.

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-'use strict'
+import $ from "jquery"
+import myObj from "./data.json"
 
-import $ from 'jquery'
-import myObj from './data.json'
+$(function() {
+  $("a.hideMe").click(function() {
+    $("#aR0").hide()
+  })
+  $("a.showMe").click(function() {
+    $("#aR0").show()
+  })
 
-$(function(){
-	$("a.hideMe").click(function(){ $("#aR0").hide(); });
-	$("a.showMe").click(function(){ $("#aR0").show(); });
-
-  document.getElementById(myObj.nomMachine).innerHTML = "<a href='" + myObj.urlCanonique + "'>" + myObj.nom + "</a>";
-});
+  document.getElementById(myObj.nomMachine).innerHTML =
+    "<a href='" + myObj.urlCanonique + "'>" + myObj.nom + "</a>"
+})
