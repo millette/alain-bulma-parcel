16c4783b39ec8d87aff8d7f9d4fd7632c62f6fdf
Robin Millette <robin@millette.info>
2018-12-19 23:17:32 -0500

    useCapture is false by default.

--- main.js
+++ main.js
@@ -4,14 +4,10 @@ const elJson = document.getElementById(myObj.nomMachine)
 elJson.innerHTML = `<a href="${myObj.urlCanonique}">${myObj.nom}</a>`
 
 const clicker = (sel, fn) =>
-  document.querySelector(sel).addEventListener(
-    "click",
-    (ev) => {
-      ev.preventDefault()
-      fn(ev)
-    },
-    false,
-  )
+  document.querySelector(sel).addEventListener("click", (ev) => {
+    ev.preventDefault()
+    fn(ev)
+  })
 
 const headerRow = document.getElementById("aR0")
 clicker("a.hideMe", () => headerRow.classList.add("is-hidden"))
